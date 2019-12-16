73c30448941fbd35f4f256b8741b5bb442058ac6
Shashi <shashi.sadasivan@gmail.com>
2019-12-17 10:25:01 +1300

    0.1.0.2

--- SSD365VSAddIn/SSD365VSAddIn/Properties/AssemblyInfo.cs
+++ SSD365VSAddIn/SSD365VSAddIn/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyFileVersion("0.1.0.1")]
+[assembly: AssemblyFileVersion("0.1.0.2")]
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SSD365VSAddIn")]
+[assembly: AssemblyTitle("SS D365 VS AddIn")]
 [assembly: AssemblyDescription("Visual studio addin for Dynamics 365 Finance and utilities")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Shashi Sadasivan")]
