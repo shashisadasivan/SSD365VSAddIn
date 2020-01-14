9e24c008ba093737f268964c0e4238419afff282
Shashi <shashi.sadasivan@gmail.com>
2020-01-15 12:18:49 +1300

    Version updated to 0.1.0.3

--- SSD365VSAddIn/SSD365VSAddIn/Properties/AssemblyInfo.cs
+++ SSD365VSAddIn/SSD365VSAddIn/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyFileVersion("0.1.0.2")]
+[assembly: AssemblyFileVersion("0.1.0.3")]
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
